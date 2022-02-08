91529d41dc6dc87a58a110e957bb4be4e9a9a645
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-08 18:34:28 +0400

    + Refactor model with computed properties

--- Concentration/Concentration/Controllers/ViewController.swift
+++ Concentration/Concentration/Controllers/ViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     //MARK: --> Properties
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
     
     var flipCount = 0 {
         didSet {
--- Concentration/Concentration/Models/Concentration.swift
+++ Concentration/Concentration/Models/Concentration.swift
@@ -9,7 +9,26 @@ import Foundation
 
 class Concentration {
     var cards = [Card]()
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     
     func chooseCard(at index: Int) {
@@ -21,13 +40,7 @@ class Concentration {
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
-                // Either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
